ee36b93352925ec42bf040fed0c518d07bba6076
xiexuefeng <1158231926@qq.com>
2019-12-10 18:33:34 +0800

    try to add ts file

---
+++ libs/gl-matrix.d.ts
@@ -0,0 +1,4 @@
+// 解决gl-matrix库的报错
+declare var vec2: any
+declare var vec3: any
+declare var vec4: any
---
+++ libs/shader-program/index.d.ts
@@ -0,0 +1,100 @@
+declare class Program {}
+
+/**
+ * attribute属性和uniform属性的类型范围
+ */
+declare enum AttributeType {
+  '1f',
+  '1i',
+  '2fv',
+  '3fv',
+  'm4fv'
+}
+
+/**
+ * attribute或uniform属性的地址类型
+ */
+type LocationType = number | WebGLUniformLocation
+
+/**
+ * attribute属性的buffer信息
+ */
+interface AttributeBuffer {
+  size: number,
+  normalize: boolean,
+  stride: number,
+  offset: number,
+  bufferType: string
+}
+
+/**
+ * 预定义的buffer信息
+ */
+interface AttributeBufferDef {
+  [name: string]: AttributeBuffer
+}
+
+/**
+ * 着色器属性对应的数组缓冲信息
+ */
+interface AttributeArrayBuffer {
+  size: number,
+  normalize: boolean,
+  stride: number,
+  offset: number,
+  type: string,
+  pos: LocationType,
+  array: Float32Array,
+  buffer: WebGLBuffer
+}
+
+/**
+ * attribute和uniform属性信息
+ */
+interface AttributeItem {
+  type: string,
+  value?: number | Float32List
+}
+
+/**
+ * attribute和uniform属性的内部设置信息
+ */
+interface AttributeInfo extends AttributeItem {
+  pos: LocationType
+}
+
+/**
+ * attribute和uniform属性的配置信息
+ */
+interface AttributeConfig {
+  [props: string]: AttributeItem
+}
+
+/**
+ * 着色器项目配置信息
+ */
+interface ProgramConfig {
+  name?: string,
+  uniform?: AttributeConfig,
+  attribute?: AttributeConfig,
+  updateTime?: boolean,
+  needScreen?: boolean,
+  needMouse?: boolean,
+  loop?: boolean
+}
+
+/**
+ * 着色器项目内所有属性的地址
+ */
+interface ProgramLocation {
+  [name: string]: LocationType
+}
+
+/**
+ * 着色器项目内需要传输的数组信息
+ */
+interface ProgramInfo {
+  vertices?: number[],
+  colors?: number[],
+  normals?: number[]
+}
